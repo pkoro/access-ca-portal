b53dfae596194df4197b06d3a5421e3c797be3de
Nikos Triantafillidis <ntrianta@lab.grid.auth.gr>
2013-12-17 15:20:30 +0200

    test

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -4,15 +4,15 @@ class SiteController < ApplicationController
       params[:id] = "home"
     end
     if params[:id].class.to_s == "String"
-     #render_comatose :page=>params[:id], :layout => true
-     render 'register/index.rhtml'
+     render_comatose :page=>params[:id], :layout => true
+     #render 'register/index.rhtml'
     elsif params[:id].class.to_s == "Array"
       if I18n.available_locales.collect {|locale| locale.to_s}.member?(params[:id][0])
         I18n.locale = params[:id][0]
         params[:id].delete_at(0)
       end
-     #render_comatose :page=>params[:id].join("/"), :layout => true
-     render 'register/index.rhtml'
+     render_comatose :page=>params[:id].join("/"), :layout => true
+     #render 'register/index.rhtml'
     else
       render :text=>"Failed?" + params[:id].class.to_s
     end
